3dd571e1dcc315950c78fb7d90730b629b506cce
Miguel Figuera <hola.digitallab@gmail.com>
2025-02-22 02:59:53 -0400

    create a profile card for the index of jugadores

--- app/(userPages)/jugadores/page.tsx
+++ app/(userPages)/jugadores/page.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { index } from "@/lib/firebase/collections/profiles";
+import ProfileCard from "@/components/profile/ProfileCard";
 
-function Page() {
-  return <div className="container">Pagina para Listar a los jugadores</div>;
+async function Page() {
+  const profiles = await index();
+
+  const profileCards = profiles.map((profile) => (
+    <ProfileCard key={profile.id} profile={profile} />
+  ));
+
+  return (
+    <div className="container">
+      <h1 className="text-center fw-bold mt-5 mb-3 pt-5 pb-3">Jugadores</h1>
+      <div className="row">{profileCards}</div>
+    </div>
+  );
 }
 
 export default Page;
---
+++ components/profile/ProfileCard.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+
+export interface Profile {
+  id: string;
+  name?: string;
+  lastname?: string;
+  address?: string;
+  age?: number;
+  status?: string;
+  role?: string;
+  yearsInTheGame?: number;
+  biography?: string;
+  zipcode?: string;
+  phone?: string;
+  email?: string;
+  style?: string;
+  photoUrl?: string;
+  country?: string;
+  [key: string]: any; // To allow potential other properties
+}
+
+interface ProfileCardProps {
+  profile: Profile;
+}
+
+const ProfileCard: React.FC<ProfileCardProps> = ({ profile }) => {
+  return (
+    <div
+      className="card h-100 d-flex flex-row mb-3 rounded-3 mx-auto"
+      style={{ width: "75%" }}
+    >
+      {profile.photoUrl ? (
+        <img
+          src={profile.photoUrl}
+          className="card-img-left img-fluid rounded-5"
+          alt="Foto de Perfil"
+          style={{ objectFit: "cover", width: "100px", height: "100px" }}
+        />
+      ) : (
+        <div
+          className="d-flex align-items-center justify-content-center bg-secondary text-white rounded-5 my-4"
+          style={{ width: "100px", height: "100px" }}
+        >
+          Sin Foto
+        </div>
+      )}
+      <div className="card-body">
+        <h5 className="card-title">
+          {profile.name} {profile.lastname}
+        </h5>
+        {profile.country && (
+          <p className="card-text">
+            <strong>País:</strong> {profile.country}
+          </p>
+        )}
+        {profile.status && (
+          <p className="card-text">
+            <strong>Estado:</strong> {profile.status}
+          </p>
+        )}
+        {profile.phone && (
+          <p className="card-text">
+            <strong>Teléfono:</strong> {profile.phone}
+          </p>
+        )}
+        {profile.role && (
+          <p className="card-text">
+            <strong>Rol:</strong> {profile.role}
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ProfileCard;
