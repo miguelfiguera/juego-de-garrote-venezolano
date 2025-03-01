259dc7aba6a10c7be5354e8de4a03c2b01cbc05a
Miguel Figuera <hola.digitallab@gmail.com>
2025-03-01 19:47:28 -0400

    add link to admin/profiles/page so we can navigate to /admin/profiles/search

--- app/admin/profiles/page.tsx
+++ app/admin/profiles/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { index } from "@/lib/firebase/collections/profiles";
 import ProfileForAdmin from "@/components/profile/ProfileForAdmin";
+import Link from "next/link";
 
 async function Page() {
   const profiles = await index();
@@ -11,6 +12,9 @@ async function Page() {
   return (
     <div className="container">
       <h1 className="text-center my-4 mx-4 border-bottom">Usuarios</h1>
+      <Link href="/admin/profiles/search" className="text-center fs-6">
+        <button className="btn btn-secondary">Buscar usuario por email</button>
+      </Link>
 
       <div className="row">{profileCards}</div>
     </div>
