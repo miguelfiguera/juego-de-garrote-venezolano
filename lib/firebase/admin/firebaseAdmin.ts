1f6d29e0987319b9d363dc5ad6c70f828d9a0284
Miguel Figuera <hola.digitallab@gmail.com>
2025-03-04 01:21:51 -0400

    modify firebaseAdmin.ts to use env variables

--- lib/firebase/admin/firebaseAdmin.ts
+++ lib/firebase/admin/firebaseAdmin.ts
@@ -1,16 +1,26 @@
 import admin from "firebase-admin";
-import secrets from "@/secret.json";
+//import secrets from "@/secret.json";
 
-const serviceAccount = secrets;
+//const serviceAccount = secrets;
 
 try {
   if (admin.apps.length > 0) {
     // Firebase app already initialized
     admin.app();
   } else {
-    // Initialize Firebase app
+    // Initialize Firebase app with secrets.json
+    //  admin.initializeApp({
+    //  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+    // });
+
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+      credential: admin.credential.cert({
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY,
+      }),
     });
   }
 } catch (error) {
